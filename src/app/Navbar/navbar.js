9d5ca523e64362134829019d8341b23bab7a9f58
Amod Ranjan <amod@Amods-MacBook-Pro.local>
2024-09-19 22:26:44 +0530

    added navbar

--- src/app/Navbar/navbar.js
+++ src/app/Navbar/navbar.js
@@ -1,24 +1,25 @@
+import { TbUserHexagon } from "react-icons/tb";
+import { IoIosArrowDown } from "react-icons/io";
 export default function Navbar(){
+    const navbarOptions = ["Home","Questions","Contest","Discussion"]
     return(
-        <div className="flex w-">
+        <div className="flex w-[100vw] h-[7vh] bg-red-500 justify-between items-center">
             <div>logo</div>
-            <div className="flex justify-between">
-                <div>
-                    <h2>Home</h2>
+            <div className="flex bg-yellow-200 w-[80vw] h-[7vh] items-center justify-end pr-10 ">
+                {navbarOptions.map((option)=>{
+                    return(
+                        <div className="w-fit h-fit bg-black p-[1vh] m-1 items-center 
+                        left-auto right-0 rounded-[10px]">
+                            <h2 className="text-white font-bold text-[2vmin]">
+                                {option}
+                            </h2>
+                        </div>
+                    )
+                })}
+                <div className="flex items-center bg-white p-1 rounded-[10px]">
+                    <TbUserHexagon className="size-[5vmin]"/>
+                    <IoIosArrowDown className="size-[2.5vmin]"/>
                 </div>
-                <div>
-                    <h2>Questions</h2>
-                </div>
-                <div>
-                    <h2>Contest</h2>
-                </div>
-                <div>
-                    <h2>Discussion</h2>
-                </div>
-            </div>
-            <div>
-                <div>User</div>
-                <div>arrowdown</div>
             </div>
         </div>
     )
